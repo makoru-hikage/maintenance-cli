817e01cd8b4091330b9b2d5479140130739b84c5
Kurt M. Ma. Coll <kurt_ma.coll@live.com>
2021-10-12 14:25:58 +0800

    Update Index and add Logout button

--- src/Index.tsx
+++ src/Index.tsx
@@ -3,23 +3,26 @@ import { useState } from 'react'
 import { FloorAreaTable } from './components/FloorAreaTable'
 import { FloorArea } from './components/FloorAreaTableRow'
 
-let perhaps: FloorArea = {
-  areaCode: "perhaps",
-  description: "perhaps",
-  floor: "perhaps",
-  row: 0,
-  column: 0
-}
+let placeholder: Array<FloorArea> = [{
+  areaCode: "0000",
+  description: "A descript or human-readable name",
+  floor: "The Floor's Name",
+  row: 1,
+  column: 1
+}]
 
 function App() {
 
   return (
   <div className="mx-16">
+    <div id="navbar" className="flex flex-row-reverse flex-wrap">
+      <button className="text-black dark:text-white p-2">Logout</button>
+    </div>
     <div id="top-buttons">
-      <button className="text-black dark:text-white p-4">Add</button>
+      <button className="text-black dark:text-white px-2 m-2 border">Add</button>
     </div>
     <div id="table-container">
-      <FloorAreaTable floorAreas={[perhaps]}/>
+      <FloorAreaTable floorAreas={placeholder}/>
     </div>
   </div>
   )
